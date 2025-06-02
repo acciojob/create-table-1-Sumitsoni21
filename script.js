54d84ac7abfdbcca437f6b01509899567e28783d
Sumitsoni21 <128276751+Sumitsoni21@users.noreply.github.com>
2025-06-03 01:13:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,28 @@ function insert_Row() {
 
 	 const table = document.getElementById("sampleTable");
 
- 
-  const newRow = document.createElement("tr");
+ const newRow = table.insertRow(0); 
+
+
+  const cell1 = newRow.insertCell(0);
+  const cell2 = newRow.insertCell(1);
+
+  cell1.innerText = "New Cell1";
+  cell2.innerText = "New Cell2";
+
+
+	
+  // const newRow = document.createElement("tr");
 
   
-  const cell1 = document.createElement("td");
-  cell1.textContent = "New Cell1";
-  const cell2 = document.createElement("td");
-  cell2.textContent = "New Cell2";
+  // const cell1 = document.createElement("td");
+  // cell1.textContent = "New Cell1";
+  // const cell2 = document.createElement("td");
+  // cell2.textContent = "New Cell2";
 
-  newRow.appendChild(cell1);
-  newRow.appendChild(cell2);
-  table.insertBefore(newRow, table.firstElementChild);
+  // newRow.appendChild(cell1);
+  // newRow.appendChild(cell2);
+  // table.insertBefore(newRow, table.firstElementChild);
 
   
 }
